98af2aacfa573d8afa0dcb1271b7fc8ccb07b3ff
Christian Sigg <christian@sigg-iten.ch>
2013-04-17 08:16:22 +0200

    fixed typos

--- inst/nscumcomp_examples.R
+++ inst/nscumcomp_examples.R
@@ -2,17 +2,18 @@ library(MASS)
 
 set.seed(1)
 
-# Regular PCA, with tolerance set to return three PCs
+# Regular PCA, with tolerance set to return five PCs
 prc <- prcomp(Boston, tol = 0.35, scale. = TRUE)
 prc
 
-# Sparse cumulative PCA with three components and 15 non-zero loadings
+# Sparse cumulative PCA with five components and 30 non-zero loadings
 # of the rotation matrix. The orthonormality penalty is set to a value which
 # avoids co-linear principal axes. Note that for this example (as in general), 
 # the non-zero loadings are not distributed uniformly over the components.
 scc <- nscumcomp(Boston, ncomp = 5, k = 30, gamma = 1e11, scale. = TRUE)  
 scc
 t(scc$rotation)%*%scc$rotation  # quasi-orthogonality of the PAs
+sum(abs(scc$rotation) > 0 )
 
 # Non-negative sparse cumulative PCA
 nscc <- nscumcomp(Boston, ncomp = 5, nneg = TRUE, k = 30, gamma = 1e4, 
@@ -21,4 +22,4 @@ nscc
 t(nscc$rotation)%*%nscc$rotation  # quasi-orthogonality of the PAs
 sum(abs(nscc$rotation) > 0 )  # k is only an upper bound
 
-c(sum(prc$sdev), sum(scc$sdev), sum(nscc$sdev))  # cumulative variances
+sapply(list(prc$sdev, scc$sdev, nscc$sdev), sum)  # cumulative variances
--- inst/nsprcomp_examples.R
+++ inst/nsprcomp_examples.R
@@ -9,7 +9,9 @@ prcomp(Boston, tol = 0.36, scale. = TRUE)
 # is derived from the length of vector k.
 nsprcomp(Boston, k = c(13,7,5,5,5), scale. = TRUE)  
 
-# Non-negative sparse PCA with subset removal as the deflation method
-# (note that the parameter k only specifies an upper bound on the PA cardinality)
+# Non-negative sparse PCA with subset removal as the deflation method. Note that
+# the feature space is exhausted after four components due to the subset
+# removal, and that the parameter k only specifies an upper bound on the 
+# cardinality of the PAs.
 nsprcomp(Boston, k = c(6,5,5,5,5), nneg = TRUE, deflation = "remove",
          scale. = TRUE)  
